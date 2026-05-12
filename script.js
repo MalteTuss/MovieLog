ae0e10005efe9761f6970313dbc648d56373e1d9
MalteTuss <maltekardell@icloud.com>
2026-05-12 20:32:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
 const API_KEY = "d133f3d52325736c0359bfd16cf21ca0";
 
 async function searchMovies() {
-  const query = document.getElementById("searchInput").value;
-
-  const res = await fetch(
-    `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`
-  );
-
-  const data = await res.json();
-
-  displayMovies(data.results);
-}
-
-async function searchMovies() {
   console.log("SEARCH CLICKED");
 
   const query = document.getElementById("searchInput").value;
